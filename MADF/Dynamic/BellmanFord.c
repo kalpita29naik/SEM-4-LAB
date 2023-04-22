1330a77ff69b9b9436b4d4e293a86ebcc0937e78
kalpita29naik <kalpita03naik@gmail.com>
2023-04-22 18:46:22 +0530

    done!

--- MADF/Dynamic/BellmanFord.c
+++ MADF/Dynamic/BellmanFord.c
@@ -1,5 +1,7 @@
+//3d.Bellman Ford Algorithm
 #include<stdio.h>
 void BellmanFord(int n,int cost[n][n],int v,int dist[n]);
+void print(int n,int dist[n]);
 
 int main(void)
 {
@@ -14,15 +16,10 @@ int main(void)
   for(int j=0;j<n;j++)
   {
    if(j==0)
-   {
-    cost[i][j]=0;
-   }
+   { cost[i][j]=0;  }
    else
-   {
-    cost[i][j]=999;
-   } 
+   { cost[i][j]=999;  } 
   }
-
  }
  printf("Enter the paths and cost:\n");
  for(int i=0;i<e;i++)
@@ -34,40 +31,37 @@ int main(void)
   scanf("%d",&v);
   int dist[n];
   BellmanFord(n,cost,v,dist);
-
 }
 
 void BellmanFord(int n,int cost[n][n],int v,int dist[n])
 {
  int i,j,k;
  for(i=0;i<n;i++)
+ { dist[i]=cost[v-1][i]; }
+ printf("dist(k)\t");
+ for (i=1;i<=n;i++)
+ { printf("%d\t",i); }
+ printf("\n");
+ printf("1\t");
+ print(n,dist);
+ for(k = 0;k < n - 2;k++)
  {
-  dist[i]=cost[v-1][i];
- }
- for(k=0;k<n;k++)
- {
-  for(i=0;i<n;i++)
+  for(i = 0;i < n;i++)
   {
-   for(j=0;j<n;j++)
+   for(j = 0;j < n;j++)
    {
-    if(dist[j]>dist[i]+cost[i][j])
-    {
-     dist[j]=dist[i]+cost[i][j];
-    }
+    if(dist[j] > dist[i] + cost[i][j])
+    { dist[j] = dist[i] + cost[i][j]; }
    }
   }
-  /*printf("The distance matrix after %d iteration is:\n",k+1);
-  for(i=0;i<n;i++)
-  {
-   printf("%d -> %d => %d",v,i+1,dist[i]);
-   printf("\n");
-  }*/
-
+  printf("%d\t",k+2);
+  print(n,dist);
  }
- printf("The final distance matrix is:\n");
- for(i=0;i<n;i++)
- {
-  printf("%d -> %d => %d",v,i+1,dist[i]);
-  printf("\n");
 }
+
+void print(int n, int dist[n])
+{
+ for(int i = 0;i < n;i++)
+ { printf("%d\t",dist[i]);}
+ printf("\n");
 }
